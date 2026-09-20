7387bc1d4ccb8d26fe6df9f4ba7cc339362ca502
WAIRIADEV <antonywairia536@gmail.com>
2026-09-20 10:09:27 +0300

    used dark and light mode

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple,brightness: Brightness.dark),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
