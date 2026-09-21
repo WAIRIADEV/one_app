452feea2eb0328e3595cee8793d062c643612501
WAIRIADEV <antonywairia536@gmail.com>
2026-09-21 15:33:17 +0300

    `fix: remove orange child container and restore valid widget structure`

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,7 @@ class MyHomePage extends StatelessWidget {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
       body: Center(
         child: Container(
@@ -77,19 +78,6 @@ class MyHomePage extends StatelessWidget {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(25),
             color: Colors.red,
-          ),
-          child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            padding: const EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: Colors.orange,
-            ),
-            child: const Text(
-              'Hello',
-              style: TextStyle(color: Colors.black),
-            ),
           ),
         ),
       ),
